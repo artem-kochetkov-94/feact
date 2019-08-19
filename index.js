508a37be60071099de47a340a5f016aa00c8547e
Artem Kochetkov <a.kochetkov@sport-express.ru>
2019-08-19 18:48:01 +0300

    feact dom component

--- index.js
+++ index.js
@@ -17,3 +17,21 @@ const Feact = {
         return componentInstance.mountComponent(container);
     }
 };
+
+class FeactDOMComponent {
+    constructor(element) {
+        this._currentElement = element;
+    }
+
+    mountComponent(container) {
+        const domElement = document.createElement(this._currentElement.type);
+        const text = this._currentElement.props.children;
+        const textNode = document.createTextNode(text);
+        domElement.appendChild(textNode);
+
+        container.appendChild(domElement);
+
+        this._hostNode = domElement;
+        return domElement;
+    }
+}
